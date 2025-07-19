efc71cba05ed785b745de3721d662a9e35d70f1b
abudi-09 <Abdsalih229@gmail.com>
2025-07-19 21:31:42 +0300

    update on the auth

--- backend/server.js
+++ backend/server.js
@@ -4,12 +4,10 @@ import { connectDB } from "./config/db.js";
 dotenv.config();
 import authRoutes from "./routes/auth.route.js";
 import { ENV_VARS } from "./config/envVars.js";
-
 const app = express();
 const PORT = ENV_VARS.PORT;
 app.use(express.json());
 app.use("/api/v1/auth", authRoutes);
-app.use("/api/v1/movie", authRoutes);
 app.listen(PORT, () => {
   console.log("server started at http://localhost:" + PORT);
   connectDB();
