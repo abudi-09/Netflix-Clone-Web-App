13868ff06ac26d902a2fa014d831d2cc697ad7e2
abudi-09 <Abdsalih229@gmail.com>
2025-07-20 22:21:32 +0300

    add protectRoute for auth

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 import express from "express";
+import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
 dotenv.config();
@@ -6,12 +7,15 @@ import authRoutes from "./routes/auth.route.js";
 import movieRoutes from "./routes/movie.route.js";
 import tvRoutes from "./routes/tv.route.js";
 import { ENV_VARS } from "./config/envVars.js";
+import { protectRoute } from "./middleware/protectRoute.js";
+
 const app = express();
 const PORT = ENV_VARS.PORT;
 app.use(express.json());
+app.use(cookieParser());
 app.use("/api/v1/auth", authRoutes);
-app.use("/api/v1/movie", movieRoutes);
-app.use("/api/v1/tv", tvRoutes);
+app.use("/api/v1/movie", protectRoute, movieRoutes);
+app.use("/api/v1/tv", protectRoute, tvRoutes);
 app.listen(PORT, () => {
   console.log("server started at http://localhost:" + PORT);
   connectDB();
