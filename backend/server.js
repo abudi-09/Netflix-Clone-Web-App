080503e0028de8cd8aec8e222f4ba704ab5bf3e5
abudi-09 <Abdsalih229@gmail.com>
2025-07-20 23:28:37 +0300

    sreach endipoint

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ dotenv.config();
 import authRoutes from "./routes/auth.route.js";
 import movieRoutes from "./routes/movie.route.js";
 import tvRoutes from "./routes/tv.route.js";
+import searchRoutes from "./routes/search.route.js";
 import { ENV_VARS } from "./config/envVars.js";
 import { protectRoute } from "./middleware/protectRoute.js";
 
@@ -16,6 +17,7 @@ app.use(cookieParser());
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/movie", protectRoute, movieRoutes);
 app.use("/api/v1/tv", protectRoute, tvRoutes);
+app.use("/api/v1/search", protectRoute, searchRoutes);
 app.listen(PORT, () => {
   console.log("server started at http://localhost:" + PORT);
   connectDB();
