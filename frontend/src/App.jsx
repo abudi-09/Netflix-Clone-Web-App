243736c4451ae941fd4f9087a3a68c111fcd44ee
abudi-09 <Abdsalih229@gmail.com>
2025-07-21 20:22:57 +0300

    UI of signUp and login page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,14 @@
-import { Routes } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
-import SignupPage from "./pages/SignupPage";
+import SignUpPage from "./pages/SignupPage";
 import HomePage from "./pages/HomePage";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
-      <Route path="/" element={<LoginPage />} />
-      <Route path="/" element={<SignupPage />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/signup" element={<SignUpPage />} />
     </Routes>
   );
 }
