baa9394f6cb5f8552ae9ae0fc08a920389ab5376
abudi-09 <Abdsalih229@gmail.com>
2025-07-22 21:28:46 +0300

    Add i gobal state for check auth

--- frontend/src/store/authUser.js
+++ frontend/src/store/authUser.js
@@ -5,6 +5,7 @@ export const useAuthStore = create((set) => ({
   user: null,
   isSigningUp: false,
   isCheckingAuth: true,
+  isLoggingOut: false,
   signup: async (credentials) => {
     set({ isSigningUp: true });
     try {
@@ -17,7 +18,18 @@ export const useAuthStore = create((set) => ({
     }
   },
   login: async () => {},
-  logout: async () => {},
+  logout: async () => {
+    set({ isLoggingOut: true });
+    try {
+      await axios.post("/api/v1/auth/logout");
+      set({ user: null, isLoggingOut: false });
+      toast.success("logout successfully");
+    } catch (error) {
+      set({ isLoggingOut: false });
+
+      toast.error(error.response.data.message || "logout falied ");
+    }
+  },
   authCheck: async () => {
     try {
       const response = await axios.get("/api/v1/auth/authCheck");
