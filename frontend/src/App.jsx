34f8341a99f9c57630351906cc1572a62b84f8ad
abudi-09 <Abdsalih229@gmail.com>
2025-07-26 12:53:47 +0300

    completed the search page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,7 +8,7 @@ import { Toaster } from "react-hot-toast";
 import { useAuthStore } from "./store/authUser";
 import { useEffect } from "react";
 import { Loader } from "lucide-react";
-//import SearchPage from "./pages/SearchPage";
+import SearchPage from "./pages/SearchPage";
 //import SearchHistoryPage from "./pages/SearchHistoryPage";
 //import NotFoundPage from "./pages/404";
 
@@ -45,6 +45,10 @@ function App() {
         <Route
           path="/watch/:id"
           element={user ? <WatchPage /> : <Navigate to={"/login"} />}
+        />
+        <Route
+          path="/search"
+          element={user ? <SearchPage /> : <Navigate to={"/login"} />}
         />
       </Routes>
       <Footer />
